24cbd96c7931c16cba0bd4ea587bf87e35b7a5e3
sonnhfit <sonnhfit@gmail.com>
2020-03-07 12:20:53 +0700

    oke

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -5,4 +5,7 @@ from .models import NewsPost
 class GetAllCourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = NewsPost
-        fields = ('id', 'title')
+        fields = ('id', 'title', 'desc', 'gia_tien', 'dientich', 'sotang', 
+        'so_phong_ngu', 'co_mat_tien', 'mattien', 'dia_chi', 'trang_thai_nha', 
+        'co_nha_bep', 'co_phong_an', 'co_san_thuong', 'co_cho_de_xe_hoi', 'loai_tin', 'loai_bds', 'phaply'
+        )
--- apps/core/views.py
+++ apps/core/views.py
@@ -21,3 +21,5 @@ class GetAllNewsPost(APIView):
         data1 = NewsPost.objects.all()
         mydata = GetAllCourseSerializer(data1, many=True)
         return Response(data=mydata.data, status=status.HTTP_200_OK)
+
+
